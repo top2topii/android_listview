648c2d0ded38fec85f96c6448a28708542f198e5
Joseph Han <top2topii@gmail.com>
2020-04-22 20:18:30 +0900

    ViewHolder 패턴 적용

--- app/src/main/java/com/example/listview/MainActivity.kt
+++ app/src/main/java/com/example/listview/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -28,7 +29,17 @@ class MainActivity : AppCompatActivity() {
 
         private val mContext: Context = context
         private val names = arrayListOf<String>(
-            "세종대왕", "흥선대원군", "정조"
+            "세종대왕", "흥선대원군", "정조", "이방원", "이순신",
+            "세종대왕", "흥선대원군", "정조", "이방원", "이순신",
+            "세종대왕", "흥선대원군", "정조", "이방원", "이순신",
+            "세종대왕", "흥선대원군", "정조", "이방원", "이순신",
+            "세종대왕", "흥선대원군", "정조", "이방원", "이순신",
+            "세종대왕", "흥선대원군", "정조", "이방원", "이순신",
+            "세종대왕", "흥선대원군", "정조", "이방원", "이순신",
+            "세종대왕", "흥선대원군", "정조", "이방원", "이순신",
+            "세종대왕", "흥선대원군", "정조", "이방원", "이순신",
+            "세종대왕", "흥선대원군", "정조", "이방원", "이순신",
+            "세종대왕", "흥선대원군", "정조", "이방원", "이순신"
         )
 
         override fun getCount(): Int {
@@ -45,23 +56,31 @@ class MainActivity : AppCompatActivity() {
 
         override fun getView(position: Int, convertView: View?, viewGroup: ViewGroup?): View {
 
-            val layoutInflater = LayoutInflater.from(mContext)
-            val rowMain = layoutInflater.inflate(R.layout.row_main, viewGroup, false)
+            val rowMain: View
 
-            val nameTextView = rowMain.findViewById<TextView>(R.id.name_textView)
-            nameTextView.text = names.get(position)
+            if(convertView == null) {
+                val layoutInflater = LayoutInflater.from(mContext)
+                rowMain = layoutInflater.inflate(R.layout.row_main, viewGroup, false)
 
-            val positionTextView = rowMain.findViewById<TextView>(R.id.position_textView)
-            positionTextView.text = "Row Number: $position"
+                Log.v("getView", "calling findViewById")
+                val nameTextView = rowMain.findViewById<TextView>(R.id.name_textView)
+                val positionTextView = rowMain.findViewById<TextView>(R.id.name_textView)
+                val viewHolder = ViewHolder(nameTextView, positionTextView)
+
+                rowMain.tag = viewHolder
+
+            } else {
+                rowMain = convertView
+            }
+
+            val viewHolder = rowMain.tag as ViewHolder
+            viewHolder.nameTextView.text = names.get(position)
+            viewHolder.positionTextView.text = "Row number: $position"
 
             return rowMain
-
         }
-//            val textView = TextView(mContext)
-//            textView.text = "my LISTVIEW ROW"
-//
-//            return textView
-//        }
+
+        private class ViewHolder(val nameTextView: TextView, val positionTextView: TextView)
     }
 }
 
